c11b8adc896c484fccac257e0e3f97ce8958e37f
MOISE-FOFOU <moise2kuete@gmail.com>
2024-05-26 23:17:07 +0100

    projet avec la configuration docker

--- model/db.js
+++ model/db.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-// URL de connexion à votre base de données MongoDB
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/DEPARTEMENT_BD';
+// Utilisez la variable d'environnement MONGO_URL ou une URL par défaut
+const MONGODB_URI ='mongodb://mongo:27017/DEPARTEMENT_BD';
 
 // Fonction de connexion à MongoDB
 async function connectDB() {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log('Connexion à MongoDB réussie !');
     } catch (error) {
         console.error('Erreur de connexion à MongoDB :', error);
