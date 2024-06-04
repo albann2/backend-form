11bca4ae206340f880a4ca8aed5fbc8f29fd9eb1
FOFOU KUETE MOISE <129035192+MOISE-FOFOU@users.noreply.github.com>
2024-06-04 14:16:35 +0100

    nouveu

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,7 +2,7 @@ const model = require('../model/modeles');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = '1111';
 
-
+const rendu=""
 
 
 // Fonction pour créer les groupes par défaut s'ils n'existent pas déjà
@@ -135,13 +135,13 @@ const updateIsActive = (Model, groupName, fieldName) => async (req, res) => {
 
 // User management
 exports.Signup = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password,departement } = req.body;
     try {
         const existingUser = await model.User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
-        const newUser = new model.User({ email, password });
+        const newUser = new model.User({ email, password,departement });
         await newUser.save();
         await exports.Signin(req, res);  // Auto-login after signup
     } catch (error) {
@@ -160,10 +160,12 @@ exports.Signin = async (req, res) => {
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
+        rendu=user.departement
+
         const payload = { email: user.email };
         const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '2h' });
         res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 2 * 60 * 60 * 1000 });
-        res.render('informatique');
+        res.render(rendu);
     } catch (error) {
         res.status(500).json({ message: 'Error during authentication', error });
     }
