e9dd4552893330c8c263ed30b850252f353fb75b
FOFOU KUETE MOISE <129035192+MOISE-FOFOU@users.noreply.github.com>
2024-06-04 15:10:31 +0100

    new

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -169,7 +169,6 @@ exports.Signin = async (req, res) => {
 };
 
 // Routes GET
-<<<<<<< HEAD
 exports.Getmission = getAllDocuments(model.Group, 'informatique', 'missions');
 exports.Getpresentation = getAllDocuments(model.Group, rendu, 'presentations');
 exports.Gethistorique = getAllDocuments(model.Group, rendu, 'historiques');
@@ -177,15 +176,7 @@ exports.Getenseignant = getAllDocuments(model.Group, rendu, 'enseignants');
 exports.Getformation = getAllDocuments(model.Group, rendu, 'formations');
 exports.Getrealisation = getAllDocuments(model.Group, rendu, 'realisations');
 exports.Getactualite = getAllDocuments(model.Group, rendu, 'actualites');
-=======
-exports.Getmission = getAllDocuments(model.Group,'informatique', 'missions');
-exports.Getpresentation = getAllDocuments(model.Group, department, 'presentations');
-exports.Gethistorique = getAllDocuments(model.Group, department, 'historiques');
-exports.Getenseignant = getAllDocuments(model.Group, department, 'enseignants');
-exports.Getformation = getAllDocuments(model.Group, department, 'formations');
-exports.Getrealisation = getAllDocuments(model.Group, department, 'realisations');
-exports.Getactualite = getAllDocuments(model.Group, department, 'actualites');
->>>>>>> 32f26d7df0cdaea0fe04efdc568379be568069f7
+
 
 // Routes POST
 exports.Postmission = createDocument(model.Group, department, 'missions');
